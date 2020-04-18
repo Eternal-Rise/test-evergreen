e9495c65f6881a623a76afbb1b70f7043d421c01
eternal_rise <bogdan@softrize.com>
2020-04-18 09:20:18 +0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'css',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+};
