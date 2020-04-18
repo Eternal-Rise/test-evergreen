52486c4f0f9024935ae7c6671ebc0ed823adb6a3
eternal_rise <bogdan@softrize.com>
2020-04-18 09:22:50 +0300

    prepared app root

---
+++ src/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    <b-container class="py-3">
+      <router-view />
+    </b-container>
+  </div>
+</template>
+
+<style lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
+
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+import router from '@/router/index';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+import '@/assets/styles/index.scss';
+
+import App from './app.vue';
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+const axiosInstance = axios.create({
+  baseURL: 'https://reqres.in/api',
+});
+
+Vue.use(VueAxios, axiosInstance);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount('#app');
